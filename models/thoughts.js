a223125c8e650509577a1e2c19b93b6bd1fe46a0
SarahGormley <sarah.gormley39@gmail.com>
2022-07-23 16:22:48 +1000

    fixed thoughts model so it can access reactions. fixed port call on index.js

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,35 +1,5 @@
 const { Schema, Types } = require('mongoose');
 
-const thoughtsSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        maxlength: 280,
-        minlength: 1,
-
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtDate) => moment(createdAtDate).format('DD MMMM YYYY, h:mm a')
-
-    },
-    username: {
-        type: String,
-        required: true,
-
-    },
-    reactions: {
-        [reactionsSchema]
-
-    },
-}, {
-    toJSON: {
-        getters: true,
-    },
-    id: false,
-});
-
 const reactionsSchema = new Schema({
     reactionId: {
         type: Schema.Types.ObjectId,
@@ -61,6 +31,34 @@ const reactionsSchema = new Schema({
     },
     id: false,
 })
+
+const thoughtsSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1,
+
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtDate) => moment(createdAtDate).format('DD MMMM YYYY, h:mm a')
+
+    },
+    username: {
+        type: String,
+        required: true,
+
+    },
+    reactions: [reactionsSchema],
+
+}, {
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+});
 
 thoughtsSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
