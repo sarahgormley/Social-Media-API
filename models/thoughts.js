9136f20938e934b611aff744d708cdf316945b0c
SarahGormley <sarah.gormley39@gmail.com>
2022-07-23 13:05:02 +1000

    thoughts and rections schema created

---
+++ models/thoughts.js
@@ -0,0 +1,70 @@
+const { Schema, Types } = require('mongoose');
+
+const thoughtsSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1,
+
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtDate) => moment(createdAtDate).format('DD MMMM YYYY, h:mm a')
+
+    },
+    username: {
+        type: String,
+        required: true,
+
+    },
+    reactions: {
+        [reactionsSchema]
+
+    },
+}, {
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+});
+
+const reactionsSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: function() { return new Types.ObjectId() }
+
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 1,
+
+    },
+    username: {
+        type: String,
+        required: true,
+
+    },
+    createdaAt: {
+
+        type: Date,
+        default: Date.now,
+        get: (createdAtDate) => moment(createdAtDate).format('DD MMMM YYYY, h:mm a')
+
+    },
+}, {
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+})
+
+thoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+
+module.exports = thoughtsSchema;
